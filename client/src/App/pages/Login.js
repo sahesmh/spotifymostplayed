bb89c5663a8827d56b54f146b50c9bba56115f69
Shane Havers <sahesmh@outlook.com>
2019-06-23 16:18:51 +1000

    Begun implementing Spotify API access. Redirecting/callbacks need work.

--- client/src/App/pages/Login.js
+++ client/src/App/pages/Login.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom'
+require('dotenv').config();
+
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
     render() {
         return (
             <div className="App">
                 <h1>Spotify Most Played Playlist Generator</h1>
-                <button>Log in to your Spotify Account</button>
+                <button onClick={this.handleClick}>Click here to log in to your Spotify Account</button>
             </div>
         );
     }
+
+    handleClick() {
+        const request = async() => {
+            const response = await fetch('http://localhost:5000/auth/spotify', {redirect: 'follow'});
+            const resjson = await response.json();
+            console.log(resjson);
+            // Redirect to /app
+            window.location.assign(resjson.redirect_url);
+        }
+
+        request();
+    }
 }
 
-export default Login;
+export default withRouter(Login);
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const cors = require('cors');
+const queryString = require('querystring');
+const cookieParser = require('cookie-parser');
 
+const stateKey = 'spotify_auth_state';
+
+let generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
 // Create Express Application
 const app = express();
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'client/public')));
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors());
+app.use(cookieParser());
 
 // API ENDPOINTS
 // Placeholder endpoint
@@ -15,6 +33,148 @@ app.get('/api/placeholder', (req, res) => {
     res.json(list);
     console.log('Sent list of items');
 })
+
+// Spotify Login endpoint
+app.get('/auth/spotify', function(req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope = 'user-read-private user-read-email user-top-read';
+    console.log('Auth requrest recieved')
+    res.json({
+        redirect_url: 'https://accounts.spotify.com/authorize?' +
+        queryString.stringify({
+            response_type: 'code',
+            client_id: process.env.CLIENT_ID,
+            scope: scope,
+            redirect_uri: 'http://localhost:5000/auth/spotify/callback',            
+            state: state
+        })
+    })    
+});
+
+// Spotify Callback enpoint
+app.get('/auth/spotify/callback', function(req, res) {
+
+    // Request refresh and access tokens after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    console.log(req.cookies);
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    // Verify State
+    if (state === null || state !== storedState) {
+        console.log('ERR: State Mismatch.', state, storedState)
+        res.redirect('/#' +
+            queryString.stringify({
+            error: 'state_mismatch'
+        }));
+        
+    } else {
+        // Clear state
+        res.clearCookie(stateKey);
+        
+        // Construct Authorisation options
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + 
+                    (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+            },
+            json: true
+        };
+        
+        // Request tokens
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+                
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function(error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                // res.redirect('/#' +
+                //     querystring.stringify({
+                //     access_token: access_token,
+                //     refresh_token: refresh_token
+                //     }))
+                res.json({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                })
+                
+            } else {
+            res.redirect('/#' +
+                querystring.stringify({
+                error: 'invalid_token'
+                }));
+            }
+        });
+    }
+});
+
+// Get Long-Term Songs endpoint
+app.get('/generate_playlist', function(req, res) {
+    console.log("Requesting songs");
+    // Construct Authorisation Options
+    var authOptions = {
+        url: 'https://api.spotify.com/v1/me/top/tracks?' + 
+            querystring.stringify({
+                'time_range': 'long_term',
+                'limit': 50
+        }),        
+        headers: {
+            'Accept'        : 'application/json',
+            'Content-Type'  : 'application/json',
+            'Authorization' : 'Bearer ' + req.query.access_token
+        },        
+        json: true
+    };
+
+    // Request Tracks
+    request.get(authOptions, function(error, response, body) {        
+        console.log("GET Response ", response.statusCode);        
+        if (!error && response.statusCode === 200) {
+            let numTracks = body.items.length;
+            let trackData = {};
+            for (trackNum = 0; trackNum < numTracks; trackNum++) {
+                let track = {
+                    uri     : body.items[trackNum].uri,                    
+                    name    : body.items[trackNum].name
+                };
+                trackData[trackNum] = track;
+            }
+            console.log(trackData);
+            res.send({
+                trackData: trackData
+            });
+        } else {
+            console.log("Error: ", error);
+            res.redirect('/#' +
+                querystring.stringify({
+                error: 'invalid_token'
+                }));
+            }
+    });
+});
 
 // 'Any Other Request' endpoint
 app.get('*', (req,res) =>{
